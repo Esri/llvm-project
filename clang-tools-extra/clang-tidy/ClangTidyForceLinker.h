6b3b0f1ef82f3ab818c5d65e90564816e15b7fbb
Christian Venegas <cvenegas@esri.com>
2017-12-05 15:30:10 -0800

    clang-tidy: Add custom Esri header define checks to clang-tidy
    The runtimecore's coding guidelines will no longer allow implementation in header
    files in order to keep build times down, allow better link time optimizations, identify
    dead code and ensure function integrity.  A clang-tidy check is being used in order
    to identify implementation in headers which can then be flagged to prevent affending
    merges and aid developers in finding them.
    
    Signed-off-by: Christian Venegas <cvenegas@esri.com>; Dan Kalowsky <DKalowsky@esri.com>

--- clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
+++ clang-tools-extra/clang-tidy/ClangTidyForceLinker.h
@@ -45,6 +45,11 @@ extern volatile int CppCoreGuidelinesModuleAnchorSource;
 static int LLVM_ATTRIBUTE_UNUSED CppCoreGuidelinesModuleAnchorDestination =
     CppCoreGuidelinesModuleAnchorSource;
 
+// This anchor is used to force the linker to link the EsriModule.
+extern volatile int EsriModuleAnchorSource;
+static int LLVM_ATTRIBUTE_UNUSED EsriModuleAnchorDestination =
+    EsriModuleAnchorSource;
+
 // This anchor is used to force the linker to link the FuchsiaModule.
 extern volatile int FuchsiaModuleAnchorSource;
 static int LLVM_ATTRIBUTE_UNUSED FuchsiaModuleAnchorDestination =
