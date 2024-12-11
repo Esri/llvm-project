6bdc09f0cd222169c5e4fd12cae7b89dc0ed666d
Chris Warner <cwarner@esri.com>
2024-12-11 14:38:04 -0800

    clang-tidy: rewrite to work on Windows too

--- clang-tools-extra/clang-tidy/esri/ImplicitCastToSizetCheck.cpp
+++ clang-tools-extra/clang-tidy/esri/ImplicitCastToSizetCheck.cpp
@@ -17,46 +17,90 @@ namespace esri {
 
 void ImplicitCastToSizetCheck::registerMatchers(MatchFinder *Finder) {
 
+  auto SizeTType = qualType(hasDeclaration(
+    namedDecl(hasName("size_t"))
+  ));
+
   auto ConditionalWithLiterals = conditionalOperator(
     hasTrueExpression(integerLiteral()),
     hasFalseExpression(integerLiteral())
   );
 
+  auto SourceExpr = expr(
+    unless(integerLiteral()),                   // ...that isn't a literal
+    unless(has(initListExpr(                    // ...or a type initializer (`int32_t{}`)
+      has(integerLiteral()))                    // ...that contains a literal
+    )),
+
+    unless(ConditionalWithLiterals),            // ...or a ternary operator where both branches are literals
+    unless(parenExpr(                           // ...or parenthesis surrounding a
+      has(ConditionalWithLiterals)              // ...ternary operator where both branches are literals
+    )),
+
+    unless(binaryOperator(                      // ...or a subtraction of two pointers
+      hasOperatorName("-"),
+      hasLHS(hasType(isAnyPointer())),
+      hasRHS(hasType(isAnyPointer()))
+    ))
+  );
+
+  auto ImplicitIntegralCast = implicitCastExpr( //  Match implicit casts
+    hasCastKind(CK_IntegralCast)                //  ...that are integram casts
+  ).bind("implicit_cast");
+
   Finder->addMatcher(
-    implicitCastExpr(                     // Match implicit casts where...
-      isExpansionInMainFile(),            // ...the cast is in the file being examined
-      hasCastKind(CK_IntegralCast),       // ...is an integral cast (as opposed to ValueCategory cast)
-      hasType(qualType(hasDeclaration(    // ...the type being casted to
-        namedDecl(hasName("size_t"))      // ...is size_t
-      ))),                                // ...and
-      hasSourceExpression(expr(           // ...the source expression
+    implicitCastExpr(                           // Match implicit casts where...
+      isExpansionInMainFile(),                  // ...the cast is in the file being examined
+      ImplicitIntegralCast,                     // ...is an integral cast (as opposed to ValueCategory cast)
+      hasImplicitDestinationType(SizeTType),    // ...where the casted to type is size_t
+      hasSourceExpression(                      // ...and the source expression
+        SourceExpr.bind("se")                   // ...matches the SourceExpr matcher
+      )
+    ),
+    this);
 
-        unless(integerLiteral()),         // ...that isn't a literal
-        unless(has(initListExpr(          // ...or a type initializer (`int32_t{}`)
-          has(integerLiteral()))          // ...that contains a literal
-       )),
+  Finder->addMatcher(
+    binaryOperation(
+      isAssignmentOperator(),
+      hasLHS(hasType(SizeTType)),
+      hasRHS(SourceExpr.bind("se"))
+    ).bind("assignment"),
+    this);
 
-        unless(ConditionalWithLiterals),  // ...or a ternary operator where both branches are literals
-        unless(parenExpr(                 // ...or parenthesis surrounding a
-          has(ConditionalWithLiterals)    // ...ternary operator where both branches are literals
-        )),
-
-        unless(binaryOperator(            // ...or a subtraction of two pointers
-          hasOperatorName("-"),
-          hasLHS(hasType(isAnyPointer())),
-          hasRHS(hasType(isAnyPointer()))
+  Finder->addMatcher(
+    varDecl(                                    // Match any variable declarations
+      hasType(SizeTType),                       // ...of type size_t
+      hasInitializer(expr(                      // ...where the init expression
+        SourceExpr.bind("se"),                  // ...matches the SourceExpr matcher
+        unless(anyOf(                           // ...but the expression
+          ImplicitIntegralCast,                 // ...isn't an integral cast itself
+          hasDescendant(ImplicitIntegralCast)   // ...or comes from an integral cast
         ))
-      ).bind("se"))
-    ).bind("implicit_cast"),
+      ))
+    ).bind("var"),
+    this);
+
+  Finder->addMatcher(
+    callExpr(                                   // Match function calls
+      forEachArgumentWithParam(                 // ...where any of the arguments
+        declRefExpr(SourceExpr.bind("se")),     // ...matches the SourceExpr matcher
+        parmVarDecl(hasType(SizeTType))         // ...and the parameter type is size_t
+      )
+    ),
     this);
 }
 
-void ImplicitCastToSizetCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *MatchedExpr = Result.Nodes.getNodeAs<ImplicitCastExpr>("implicit_cast");
-  if (MatchedExpr->isPartOfExplicitCast())
-    return;
+void ImplicitCastToSizetCheck::check(const MatchFinder::MatchResult &Result)
+{
+  if (const auto *MatchedExpr = Result.Nodes.getNodeAs<ImplicitCastExpr>("implicit_cast"))
+  {
+    if (MatchedExpr->isPartOfExplicitCast())
+      return;
+  }
 
   const auto *SourceExpr = Result.Nodes.getNodeAs<Expr>("se");
+  assert(SourceExpr && "Needs to have a source Expr");
+
   if (Result.Context->getTypeSize(SourceExpr->getType()) <= 32)
     return;
 
@@ -85,7 +129,7 @@ void ImplicitCastToSizetCheck::check(const MatchFinder::MatchResult &Result) {
       return;
   }
 
-  diag(MatchedExpr->getBeginLoc(), "implicit cast to size_t may be a narrowing cast")
+  diag(SourceExpr->getBeginLoc(), "implicit cast to size_t may be a narrowing cast")
       << SourceExpr->getSourceRange();
 }
 
